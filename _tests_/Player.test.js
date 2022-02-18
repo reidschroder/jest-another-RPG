bf451dddb3f4bc9ecbf26014602e2f34274b6190
Reid Schroder <reidschroder2@gmail.com>
2022-02-18 17:40:46 -0500

    finish player constructor

--- _tests_/Player.test.js
+++ _tests_/Player.test.js
@@ -62,10 +62,30 @@ test("subtracts from player's health", () => {
     expect(player.health).toBe(0);
 });
 
-Player.prototype.reduceHealth = function(health) {
-    this.health -= health;
 
-    if (this.health < 0) {
-        this.health = 0;
-    }
-};
+test("gets player's attack value", () => {
+    const player = new Player('Dave');
+    player.strength = 10;
+
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+test('adds a potion to the inventory', () => {
+    const player = new Player('Dave');
+    const oldCount = player.inventory.length;
+  
+    player.addPotion(new Potion());
+  
+    expect(player.inventory.length).toBeGreaterThan(oldCount);
+  });
+
+test('uses a potion from inventory', () => {
+    const player = new Player('Dave');
+    player.inventory = [new Potion(), new Potion(), new Potion()];
+    const oldCount = player.inventory.length;
+
+    player.usePotion(1);
+
+    expect(player.inventory.length).toBeLessThan(oldCount);
+});
